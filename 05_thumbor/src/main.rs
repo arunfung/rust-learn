8d83e7f986bd66c8dcd5700b7196c75da9df219d
Arun Fung <arunfung@gmail.com>
2022-09-17 18:56:17 +0800

    Web 服务器

--- 05_thumbor/src/main.rs
+++ 05_thumbor/src/main.rs
@@ -1,5 +1,52 @@
+use anyhow::Result;
+use axum::{
+    extract::{Extension, Path},
+    routing::get,
+    http::{HeaderMap, HeaderValue, StatusCode},
+    Router,
+};
+use percent_encoding::{percent_decode_str, percent_encode, NON_ALPHANUMERIC};
+use serde::Deserialize;
+use std::convert::TryInto;
+
+// 引入 protobuf 生成的代码
 mod pb;
 
-fn main() {
-    println!("Hello, world!");
+use pb::*;
+
+// 参数使用 serde 做 Deserialize，axum 会自动识别并解析
+#[derive(Deserialize)]
+struct Params {
+    spec: String,
+    url: String,
+}
+
+#[tokio::main]
+async fn main() {
+    // 初始化 tracing
+    tracing_subscriber::fmt::init();
+    // 构建路由
+    let app = Router::new()
+        // `GET /image` 会执行 generate 函数，并把 spec 和 url 传递过去
+        .route("/image/:spec/:url", get(generate));
+
+    // 运行 web 服务器
+    let addr = "127.0.0.1:3000".parse().unwrap();
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// httpie get "http://localhost:3000/image/CgoKCAjYBBCgBiADCgY6BAgUEBQKBDICCAM/https://images.pexels.com/photos/2470905/pexels-photo-2470905.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+// 通过 httpie 尝试请求下
+// 我们就只把参数解析出来
+async fn generate(Path(Params { spec, url }): Path<Params>) -> Result<String, StatusCode> {
+    let url = percent_decode_str(&url).decode_utf8_lossy();
+    let spec: ImageSpec = spec
+        .as_str()
+        .try_into()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+    Ok(format!("url: {}\n spec: {:#?}", url, spec))
 }
