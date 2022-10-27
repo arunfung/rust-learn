d650c4dfe63848cf3536ce5581be27a9c4532f6e
arun fung <arunfung@gmail.com>
2022-10-27 16:31:08 +0800

    12_ip_1

--- 12_type_system/src/ip.rs
+++ 12_type_system/src/ip.rs
@@ -1,6 +1,7 @@
 use std::net::SocketAddr;
 
 fn main() {
+    // 在泛型函数后使用 ::<T> 来强制使用类型 T，这种写法被称为 turbofish。
     let addr = "127.0.0.1:8080".parse::<SocketAddr>().unwrap();
     println!("addr: {:?}, port: {:?}", addr.ip(), addr.port());
 }
