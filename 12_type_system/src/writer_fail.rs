9805ca870f92b792025923b45629da1df8eb56d8
arun fung <arunfung@gmail.com>
2022-10-31 23:56:09 +0800

    12_writer_fail

---
+++ 12_type_system/src/writer_fail.rs
@@ -0,0 +1,26 @@
+use std::io::{BufWriter, Write};
+use std::net::TcpStream;
+
+#[derive(Debug)]
+struct MyWriter<W> {
+    writer: W,
+}
+
+impl<W: Write> MyWriter<W> {
+    // stream 的生命周期不够长
+    // pub fn new(addr: &str) -> Self {
+    //     let stream = TcpStream::connect(addr).unwrap();
+    //     Self {
+    //         writer: BufWriter::new(stream),
+    //     }
+    // }
+
+    pub fn write(&mut self, buf: &str) -> std::io::Result<()> {
+        self.writer.write_all(buf.as_bytes())
+    }
+}
+
+fn main() {
+    // let mut writer = MyWriter::new("127.0.0.1:8080");
+    // writer.write("hello world!").unwrap();
+}
