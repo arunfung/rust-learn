922c2ffe455f16a86bb235db3580515c00868750
arun fung <arunfung@gmail.com>
2022-11-02 22:02:36 +0800

    13_parse2_err

---
+++ 13_traits/src/parse2.rs
@@ -0,0 +1,45 @@
+use std::str::FromStr;
+use regex::Regex;
+
+pub trait Parse {
+    type Error;
+    fn parse(s: &str) -> Result<Self, Self::Error> where Self: Sized;
+}
+
+// 我们约束 T 必须同时实现了 FromStr 和 Default
+// 这样在使用的时候我们就可以用这两个 trait 的方法了
+impl<T> Parse for T where T: FromStr + Default {
+    // 定义关联类型 Error 为 String
+    type Error = String;
+    fn parse(s: &str) -> Result<Self, Self::Error> {
+        let re: Regex = Regex::new(r"^[0-9]+(\.[0-9]+)?").unwrap();
+        // 当出错时我们返回 Err(String)
+        if let Some(captures) = re.captures(s) {
+            // 取第一个 match，将其捕获的内容换成 T
+            captures
+                .get(0)
+                .map_or(Err("failed to capture".to_string()), |s| {
+                    s.as_str()
+                        .parse()
+                        .map_err(|_err| "failed to parse captured string".to_string())
+                })
+        } else {
+            Err("failed to parse string".to_string())
+        }
+    }
+}
+
+#[test]
+fn parse_should_work() {
+    assert_eq!(u32::parse("123abcd"), Ok(123));
+    assert_eq!(
+        u32::parse("123.45abcd"),
+        Err("failed to parse captured string".into())
+    );
+    assert_eq!(f64::parse("123.45abcd"), Ok(123.45));
+    assert!(f64::parse("abcd").is_err());
+}
+
+fn main() {
+    println!("result: {:?}", u8::parse("255 hello world"));
+}
