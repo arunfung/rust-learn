7795d7c1d3e4b0008a6ac62943dff8fc1f6918da
arun fung <arunfung@gmail.com>
2022-11-02 20:48:04 +0800

    13_parse_f64_1

--- 13_traits/src/parse1.rs
+++ 13_traits/src/parse1.rs
@@ -9,7 +9,7 @@ impl Parse for f64 {
     fn parse(s: &str) -> Self {
         let re: Regex = Regex::new(r"^[0-9]+(\.[0-9]+)?").unwrap();
         if let Some(captures) = re.captures(s) {
-            // 取第一个 match，将其捕获的 digits 换成 u8
+            // 取第一个 match，将其捕获的 digits 换成 f64
             captures.get(0).map_or(0.0, |s| s.as_str().parse().unwrap_or(0.0))
         } else {
             0.0
@@ -24,5 +24,5 @@ fn parse_should_work() {
     assert_eq!(f64::parse("123.4abcd"), 123.4);
 }
 fn main() {
-println!("result: {}", f64::parse("255 hello world"))
+println!("result: {}", f64::parse("255.1 hello world"))
 }
