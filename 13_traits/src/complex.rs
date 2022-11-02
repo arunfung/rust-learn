31db368e8bea1c606ae032cf4cc72cc323f03773
arun fung <arunfung@gmail.com>
2022-11-02 22:42:10 +0800

    13_&complex

--- 13_traits/src/complex.rs
+++ 13_traits/src/complex.rs
@@ -18,16 +18,29 @@ impl Add for Complex {
 
     // 注意 add 第一个参数是 self，会移动所有权
     fn add(self, rhs: Self) -> Self::Output {
-        let  real = self.real + rhs.real;
+        let real = self.real + rhs.real;
         let imagine = self.imagine + rhs.imagine;
         Self::new(real, imagine)
+    }
+}
+
+// 如果不想移动所有权，可以为 &Complex 实现 add，这样可以做 &c1 + &c2
+impl Add for &Complex {
+    // 注意返回值不应该是 Self 了，因为此时 Self 是 &Complex
+    type Output = Complex;
+
+    // 注意 add 第一个参数是 self，会移动所有权
+    fn add(self, rhs: Self) -> Self::Output {
+        let real = self.real + rhs.real;
+        let imagine = self.imagine + rhs.imagine;
+        Complex::new(real, imagine)
     }
 }
 
 fn main() {
     let c1 = Complex::new(1.0, 1f64);
     let c2 = Complex::new(2 as f64, 3.0);
+    // &c1 + &c2，这样所有权就不会移动了
+    println!("{:?}", &c1 + &c2);
     println!("{:?}", c1 + c2);
-    // c1、c2 已经被移动，所以下面这句无法编译
-    // println!("{:?}", c1 + c2);
 }
