d38c95232edab094809be3301edbfdd1914e71e9
arun fung <arunfung@gmail.com>
2022-11-05 23:35:28 +0800

    trait_object

---
+++ 13_traits/src/trait_object.rs
@@ -0,0 +1,7 @@
+use std::{fs::File, io::Write};
+
+fn main() {
+    let mut f = File::create("/tmp/test_write_trait").unwrap();
+    let w: &mut dyn Write = &mut f;
+    w.write_all(b"hello ").unwrap();
+}
