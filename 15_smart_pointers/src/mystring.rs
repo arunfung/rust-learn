c5f973058b50875e617216420d7cce93cbaff35c
arun fung <arunfung@gmail.com>
2023-02-09 22:29:22 +0800

    15_mystring

---
+++ 15_smart_pointers/src/mystring.rs
@@ -0,0 +1,84 @@
+use std::{fmt, ops::Deref, str};
+use std::fmt::Formatter;
+
+const MINI_STRING_MAX_LEN: usize = 30;
+
+// MyString 里，String 有 3 个 word，共 24 字节，所以它以 8 字节对齐
+// 所以 enum 的 tag + padding 最少 8 字节，整个结构占 32 字节。
+// MiniString 可以最多有 30 字节（再加上 1 字节长度和 1字节 tag），就是 32 字节.
+struct MiniString {
+    len: u8,
+    data: [u8; MINI_STRING_MAX_LEN],
+}
+
+impl MiniString {
+    // 这里 new 接口不暴露出去，保证传入的 v 的字节长度小于等于 30
+    fn new(v: impl AsRef<str>) -> Self {
+        let bytes = v.as_ref().as_bytes();
+        // 我们在拷贝内容时一定要使用字符串的字节长度
+        let len = bytes.len();
+        let mut data = [0u8; MINI_STRING_MAX_LEN];
+        data[..len].copy_from_slice(bytes);
+        Self {
+            len: len as u8,
+            data,
+        }
+    }
+}
+
+impl Deref for MiniString {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        // 由于生成 MiniString 的接口是隐藏的，它只能来自字符串，所以下面这行是安全的
+        str::from_utf8(&self.data[..self.len as usize]).unwrap()
+        // unsafe 版本
+        // unsafe { str::from_utf8_unchecked(&self.data[..self.len as usize]) }
+    }
+}
+
+impl fmt::Debug for MiniString {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        // 这里由于实现了 Deref trait，可以直接得到一个 &str 输出
+        write!(f, "{}", self.deref())
+    }
+}
+
+#[derive(Debug)]
+enum MyString {
+    Inline(MiniString),
+    Standard(String),
+}
+
+// 实现 Deref 接口对两种不同的场景统一得到 &str
+impl Deref for MyString {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        match *self {
+            MyString::Inline(ref s) => s.deref(),
+            MyString::Standard(ref s) => s.deref(),
+        }
+    }
+}
+
+impl From<&str> for MyString {
+    fn from(s: &str) -> Self {
+        match s.len() > MINI_STRING_MAX_LEN {
+            true => Self::Standard(s.to_owned()),
+            _ => Self::Inline(MiniString::new(s)),
+        }
+    }
+}
+
+impl fmt::Display for MyString {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.deref())
+    }
+}
+
+fn main() {
+    let len1 = std::mem::size_of::<MyString>();
+    let len2 = std::mem::size_of::<MiniString>();
+    println!("Len: MyString {}, MiniString {}", len1, len2);
+}
