bff3e587e2be567c0e27df25392a002af3645e79
arun fung <arunfung@gmail.com>
2023-03-10 00:37:55 +0800

    16_String=>AsRef<[u8]>

--- 16_data_structure/src/slice4.rs
+++ 16_data_structure/src/slice4.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 fn main() {
     let s = String::from("hello");
     // &String 会被解引用成 &str
@@ -10,6 +12,12 @@ fn main() {
     print_slice1(&s);
     print_slice1(&s[..]);
     print_slice1(s.clone());
+
+    // String 也实现了 AsRef<[u8]>，所以下面的代码成立
+    // 打印出来是 [104, 101, 108, 108, 111]
+    print_slice2(&s);
+    print_slice2(&s[..]);
+    print_slice2(s);
 }
 
 fn print_slice(s: &str) {
@@ -19,3 +27,8 @@ fn print_slice(s: &str) {
 fn print_slice1<T: AsRef<str>>(s: T) {
     println!("{:?}", s.as_ref());
 }
+
+fn print_slice2<T, U>(s: T)
+    where
+        T: AsRef<[U]>,
+        U: fmt::Debug, { println!("{:?}", s.as_ref()); }
