2528099544200f6e7d9d1891ea05fd7209b9e9a9
arun fung <arunfung@gmail.com>
2023-03-10 00:32:20 +0800

    16_String=>AsRef<str>

--- 16_data_structure/src/slice4.rs
+++ 16_data_structure/src/slice4.rs
@@ -5,8 +5,17 @@ fn main() {
     // &s[..] 和 s.as_str() 一样，都会得到 &str
     print_slice(s.as_str());
     print_slice(&s[..]);
+
+    // String 支持 AsRef<str>
+    print_slice1(&s);
+    print_slice1(&s[..]);
+    print_slice1(s.clone());
 }
 
 fn print_slice(s: &str) {
     println!("{:?}", s);
+}
+
+fn print_slice1<T: AsRef<str>>(s: T) {
+    println!("{:?}", s.as_ref());
 }
