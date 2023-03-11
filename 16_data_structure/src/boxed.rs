e2761a2fa034e5b8e9fafdce4f0567867c9209fc
arun fung <arunfung@gmail.com>
2023-03-11 22:17:14 +0800

    16_boxed_change

--- 16_data_structure/src/boxed.rs
+++ 16_data_structure/src/boxed.rs
@@ -9,10 +9,15 @@ fn main() {
 
     // 从 Vec<T> 转换成 Box<[T]>，此时会丢弃多余的 capacity
     let b1 = v1.into_boxed_slice();
-    let b2 = b1.clone();
+    let mut b2 = b1.clone();
 
     let v2 = b1.into_vec();
     println!("cap should be exactly 5: {}, len: {}", v2.capacity(), v2.len());
 
     assert!(b2.deref() == v2);
+
+    // Box<[T]> 可以更改其内部数据，但无法 push
+    b2[0] = 2;
+    // b2.push(6);
+    println!("b2: {:?}", b2);
 }
