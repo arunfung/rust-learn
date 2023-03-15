6911589aca02a2187fdb167d7655202801b9c0b2
arun fung <arunfung@gmail.com>
2023-03-15 23:54:10 +0800

    17_HashMap_added 1

--- 17_hash_table/src/hashmap1.rs
+++ 17_hash_table/src/hashmap1.rs
@@ -1,3 +1,13 @@
+use std::collections::{HashMap};
+
 fn main() {
-    println!("Hello, world!");
+    let mut map = HashMap::new();
+    explain("empty", &map);
+
+    map.insert('a', 1);
+    explain("added 1", &map);
+}
+
+fn explain<K, V>(name: &str, map: &HashMap<K, V>) {
+    println!("{}: len: {}, cap: {}", name, map.len(), map.capacity());
 }
