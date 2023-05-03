f1a797ae48caaac1ee85d2c35874dede576bf29a
arun fung <arunfung@gmail.com>
2023-05-03 23:23:05 +0800

    18_defined_error

--- 18_error_handling/src/panic.rs
+++ 18_error_handling/src/panic.rs
@@ -1,4 +1,5 @@
-use std::panic;
+use std::{io, panic};
+use thiserror::Error;
 
 fn main() {
     let result = panic::catch_unwind(|| {
@@ -11,4 +12,17 @@ fn main() {
     });
     assert!(result.is_err());
     println!("panic captured: {:#?}", result);
+}
+
+#[derive(Error, Debug)]
+#[non_exhaustive]
+pub enum DataStoreError {
+    #[error("data store disconnected")]
+    Disconnect(#[from] io::Error),
+    #[error("the data for key `{0}` is not available")]
+    Redaction(String),
+    #[error("invalid header (expected {expected:?}, found {found:?})")]
+    InvalidHeader { expected: String, found: String },
+    #[error("unknown data store error")]
+    Unknown,
 }
