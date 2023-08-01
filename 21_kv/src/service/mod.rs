9756c2357addcf81885c6946e77724cd2950e57b
arun fung <arunfung@gmail.com>
2023-08-01 23:21:42 +0800

    21_service_test

--- 21_kv/src/service/mod.rs
+++ 21_kv/src/service/mod.rs
@@ -1,4 +1,8 @@
-use crate::{command_request::RequestData, *};
+use crate::{
+    command_request::RequestData, CommandRequest, CommandResponse, KvError, MemTable, Storage,
+};
+use std::sync::Arc;
+use tracing::debug;
 
 mod command_service;
 
@@ -6,6 +10,42 @@ mod command_service;
 pub trait CommandService {
     /// 处理 Command，返回 Response
     fn execute(self, store: &impl Storage) -> CommandResponse;
+}
+
+/// Service 数据结构
+pub struct Service<Store = MemTable> {
+    inner: Arc<ServiceInner<Store>>,
+}
+
+impl<Store> Clone for Service<Store> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: Arc::clone(&self.inner),
+        }
+    }
+}
+
+/// Service 内部数据结构
+pub struct ServiceInner<Store> {
+    store: Store,
+}
+
+impl<Store: Storage> Service<Store> {
+    pub fn new(store: Store) -> Self {
+        Self {
+            inner: Arc::new(ServiceInner { store }),
+        }
+    }
+
+    pub fn execute(&self, cmd: CommandRequest) -> CommandResponse {
+        debug!("Got request: {:?}", cmd);
+        // TODO: 发送 on_received 事件
+        let res = dispatch(cmd, &self.inner.store);
+        debug!("Executed response: {:?}", res);
+        // TODO: 发送 on_executed 事件
+
+        res
+    }
 }
 
 // 从 Request 中得到 Response，目前处理 HGET/HGETALL/HSET
@@ -18,6 +58,36 @@ pub fn dispatch(cmd: CommandRequest, store: &impl Storage) -> CommandResponse {
         _ => KvError::Internal("Not implemented".into()).into(),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{MemTable, Value};
+    use std::thread;
+
+    #[test]
+    fn service_should_works() {
+        // 我们需要一个 service 结构至少包含 Storage
+        let service = Service::new(MemTable::default());
+
+        // service 可以运行在多线程环境下，它的 clone 应该是轻量级的
+        let cloned = service.clone();
+
+        // 创建一个线程，在 table t1 中写入 k1, v1
+        let handle = thread::spawn(move || {
+            let res = cloned.execute(CommandRequest::new_hset("t1", "k1", "v1".into()));
+            assert_res_ok(res, &[Value::default()], &[]);
+        });
+        handle.join().unwrap();
+
+        // 在当前线程下读取 table t1 的 k1，应该返回 v1
+        let res = service.execute(CommandRequest::new_hget("t1", "k1"));
+        assert_res_ok(res, &["v1".into()], &[]);
+    }
+}
+
+#[cfg(test)]
+use crate::{Kvpair, Value};
 
 // 测试成功返回的结果
 #[cfg(test)]
