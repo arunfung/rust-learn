197418a1b6eca603e2dc2a7698d52bf7d20e44ac
arun fung <arunfung@gmail.com>
2023-11-28 19:07:38 +0800

    22_rgrep

--- 22_rgrep/src/main.rs
+++ 22_rgrep/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use rgrep::*;
+fn main() -> Result<()> {
+    let config: GrepConfig = GrepConfig::parse();
+    config.match_with_default_strategy()?;
+
+    Ok(())
 }
